317a642033e23350774aff848de350f7359d169b
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-20 05:12:29 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,36 +1,28 @@
-import React, { Component } from "react";
-import Cell from "./Cell.js";
-
+import React, { Component } from 'react';
+import Cell from './Cell' ;
 export default class Matrix extends Component {
+  
   genRow = (vals) => {
-    return vals.map((val) => <Cell value={val} />);
-  };
-
-  genMatrix = () => {
-    return this.props.values.map((rowVals) => (
-      <div className="row">{this.genRow(rowVals)}</div>
-    ));
-  };
-
-  render() {
-    return <div id="matrix">{this.genMatrix()}</div>;
-
+    return vals.map(val => <Cell className="cell" />) // replace me and render a cell component instead!
   }
+  
+  genMatrix = () => {
+    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+  }
+  
+  render() {
+    return (
+      <div id="matrix">
+        {this.genMatrix()}
+      </div>
+    )
+  }
+  
 }
+
 Matrix.defaultProps = {
-  values: (() => {
-    const defRow = [
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-      "#F00",
-    ];
-    return new Array(10).fill(defRow);
-  })(),
-};
+   values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
+}
